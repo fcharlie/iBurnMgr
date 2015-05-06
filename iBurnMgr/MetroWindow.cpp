3f349bad7f6f8f7dd29cd158edb3134ca7dffa99
Force.Charlie-I <forcegroup@users.noreply.github.com>
2015-05-06 17:36:21 +0800

    Update MetroWindow.cpp

--- iBurnMgr/MetroWindow.cpp
+++ iBurnMgr/MetroWindow.cpp
@@ -2,7 +2,7 @@
 * MetroWindow.cpp
 * Note: iBurnMgr MetroWindow
 * E-mail:<forcemz@outlook.com>
-* Data: @2015.03
+* Date: @2015.03
 * Copyright (C) 2015 The ForceStudio All Rights Reserved.
 **********************************************************************************************************/
 #include "Precompiled.h"
