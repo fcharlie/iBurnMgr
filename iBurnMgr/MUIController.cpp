d7d1f81d86e4db31b49bc1e3a9601e664d9b614c
Force.Charlie-I <forcegroup@users.noreply.github.com>
2015-05-06 17:35:51 +0800

    Update MUIController.cpp

--- iBurnMgr/MUIController.cpp
+++ iBurnMgr/MUIController.cpp
@@ -2,7 +2,7 @@
 * MUIController.cpp
 * Note: iBurnMgr MUIController
 * E-mail:<forcemz@outlook.com>
-* Data: @2015.03
+* Date: @2015.03
 * Copyright (C) 2015 The ForceStudio All Rights Reserved.
 **********************************************************************************************************/
 #include "Precompiled.h"
