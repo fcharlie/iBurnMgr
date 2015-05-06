0f97f4a8aeed7c6f6887c73ab2500ef607404e76
Force.Charlie-I <forcegroup@users.noreply.github.com>
2015-05-06 17:33:58 +0800

    Update EquipmentController.h

--- iBurnMgr/EquipmentController.h
+++ iBurnMgr/EquipmentController.h
@@ -2,7 +2,7 @@
 * EquipmentController.h
 * Note: iBurnMgr EquipmentController
 * E-mail:<forcemz@outlook.com>
-* Data: @2015.03
+* Date: @2015.03
 * Copyright (C) 2015 The ForceStudio All Rights Reserved.
 **********************************************************************************************************/
 #ifndef EQUIPMENTCONTROLLER_H
