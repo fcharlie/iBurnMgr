c2f83b0c33cb3954304532ad1898dfd6e6b2920b
Force.Charlie-I <forcegroup@users.noreply.github.com>
2015-05-06 17:36:55 +0800

    Update ResolveBootSupervisor.cpp

--- iBurnMgr/ResolveBootSupervisor.cpp
+++ iBurnMgr/ResolveBootSupervisor.cpp
@@ -2,7 +2,7 @@
 * ResolveBootSupervisor.cpp
 * Note: iBurnMgr ResolveBootSupervisor
 * E-mail:<forcemz@outlook.com>
-* Data: @2015.03
+* Date: @2015.03
 * Copyright (C) 2015 The ForceStudio All Rights Reserved.
 **********************************************************************************************************/
 #include "Precompiled.h"
