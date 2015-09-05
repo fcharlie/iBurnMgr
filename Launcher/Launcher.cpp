09555e75c214a2cb144fc8181cdff6a71aa25570
Force Charlie <charlieio@outlook.com>
2015-09-05 10:26:21 +0800

    Launcher update

--- Launcher/Launcher.cpp
+++ Launcher/Launcher.cpp
@@ -9,9 +9,12 @@
 #include <wchar.h>
 #include <Shlwapi.h>
 #include <Shlobj.h>
+#include <string>
 
 #pragma comment(lib,"Shlwapi.lib")
 #pragma comment(lib,"Shell32.lib")
+
+#define UNC_PATH 32 * 1024 - 1
 
 int WINAPI  wWinMain(
 	_In_ HINSTANCE hInstance,
@@ -20,19 +23,43 @@ int WINAPI  wWinMain(
 	_In_ int nCmdShow
 	)
 {
-	WCHAR LauncherAppPath[32 * 1024 - 1] = { 0 };
-	WCHAR LauncherAppDirectory[32 * 1024 - 1] = { 0 };
-	WCHAR Home[260] = { 0 };
-	WCHAR ifile[32 * 1024 - 1] = { 0 };
-	GetModuleFileNameW(hInstance, ifile, 32 * 1024 - 1);
-	(wcsrchr(ifile, L'.'))[1] = 0;
-	wcscat_s(ifile, L"ini");
+	WCHAR LauncherAppDirectory[MAX_PATH] = { 0 };
+	WCHAR Home[MAX_PATH] = { 0 };
+	///////
+	WCHAR ifile[UNC_PATH] = { 0 };
+	WCHAR LauncherAppPath[UNC_PATH] = { 0 };
 
+	GetModuleFileNameW(hInstance, ifile, UNC_PATH);
+	PathRemoveFileSpecW(ifile);
+	std::wstring launcherDir = ifile;
+
+	wcscat_s(ifile, L"\\launcher.ini");
+	
 	SHGetSpecialFolderPathW(NULL, Home, CSIDL_PERSONAL, FALSE);
-	(wcsrchr(Home, L'\\'))[0] = 0;
-	//MessageBox(NULL, Home, L"CSIDL_PERSONAL", MB_OK);
-	GetPrivateProfileStringW(L"Launcher", L"LauncherAppPath", L"LauncherApp.exe", LauncherAppPath, 32 * 1024 - 1, ifile);
-	GetPrivateProfileStringW(L"Launcher", L"LauncherAppDirectory", Home, LauncherAppDirectory, 32 * 1024 - 1, ifile);
-	ShellExecuteW(NULL, L"runas", LauncherAppPath, NULL, LauncherAppDirectory, SW_NORMAL);
+	if (!PathFileExistsW(ifile)){
+		MessageBoxW(nullptr, ifile, L"Not found iBurnMgr launcher configure [launcher.ini]", MB_OK | MB_ICONERROR);
+		return 1;
+	}
+	PathRemoveFileSpecW(Home);
+	GetPrivateProfileStringW(L"Launcher", L"LauncherAppPath", L"LauncherApp.exe", LauncherAppPath, UNC_PATH, ifile);
+	GetPrivateProfileStringW(L"Launcher", L"LauncherAppDirectory", Home, LauncherAppDirectory, MAX_PATH, ifile);
+	launcherDir += L"\\";
+	launcherDir += LauncherAppPath;
+	if (PathIsUNCW(LauncherAppPath));
+	else if (LauncherAppPath[1] == ':');
+	else if (PathFileExistsW(launcherDir.c_str())&&PathIsExe(launcherDir.c_str())){
+		auto ret = ShellExecuteW(NULL, L"runas", launcherDir.c_str(), NULL, LauncherAppDirectory, SW_NORMAL);
+		return ret != nullptr;
+	}else{
+		MessageBoxW(nullptr, LauncherAppPath, L"Not found vaild program ", MB_OK | MB_ICONERROR);
+		return 1;
+	}
+	if (PathFileExistsW(LauncherAppPath) && PathIsExe(LauncherAppPath)){
+		auto ret = ShellExecuteW(NULL, L"runas", LauncherAppPath, NULL, LauncherAppDirectory, SW_NORMAL);
+		return ret != nullptr;
+	}
+	else{
+		MessageBoxW(nullptr, LauncherAppPath, L"Cannot run this file", MB_OK | MB_ICONERROR);
+	}
 	return 0;
 }
